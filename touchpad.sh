518edbbec111e54c0963d62d1bfc6b0f69f84aff
Rafael_Koppe <80339726+RafaelKoppe@users.noreply.github.com>
2022-04-14 15:52:37 -0300

    Add files via upload
    tocuhpad-atualizado-shell-script

--- touchpad.sh
+++ touchpad.sh
@@ -5,8 +5,12 @@ state=`xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$"`
 
 if [ "$state" -eq '1' ];then
   xinput --disable "$device"
-  zenity --info --text "Touchpad Disabled" --timeout=2
+  notify-send --icon=/usr/share/icons/Pop/scalable/status/touchpad-disabled-symbolic.svg "Disabled" "Touchpad Desativado."  
+  #notify-send -a 'Touchpad' 'Disabled' -i input-touchpad
+  #zenity --info --text "Touchpad Disabled" --timeout=2
 else
   xinput --enable "$device"
-  zenity --info --text "Touchpad ENABLED" --timeout=2
+  notify-send --icon=/usr/share/icons/Pop/scalable/status/touchpad-enabled-symbolic.svg "Enabled" "Touchpad Ativado."
+  #notify-send -a 'Touchpad' 'Enable' -i input-touchpad
+  #zenity --info --text "Touchpad ENABLED" --timeout=2
 fi
