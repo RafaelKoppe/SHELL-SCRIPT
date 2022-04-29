b962b541899d1c4a490c7596252f4a5eab2e6e58
Rafael_Koppe <80339726+RafaelKoppe@users.noreply.github.com>
2022-04-29 13:17:40 -0300

    Add files via upload

---
+++ programas-automatizados.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+echo "instalando o wget no FEDORA"
+echo
+sudo dnf install wget
+echo
+
+echo "Instaland o OpenJDK 17 - JDK:"
+sudo dnf install java-latest-openjdk.x86_64
+
+echo "configurar a variavel de ambiente"
+sudo update-alternatives --config java
+
+echo "baixando a IDE ECLIPSE"
+wget https://eclipse.c3sl.ufpr.br/technology/epp/downloads/release/2022-03/R/eclipse-jee-2022-03-R-linux-gtk-x86_64.tar.gz
+echo
+echo "Extraindo o pacote zipado"
+tar -xvzf eclipse-jee-2022-03-R-linux-gtk-x86_64.tar.gz
+
+##Configurando o eclipse .desktop
+echo "Configurando o arquivo .desktop para o eclipse"
+echo "
+[Desktop Entry]
+Comment=Java IDE
+Terminal=false
+Name=Eclipse
+Exec=`echo $HOME`/eclipse/eclipse
+Icon=`echo $HOME`/eclipse/icon.xpm
+Type=Application
+Categories=Application;Development;
+" > `echo $HOME`/.local/share/applications/eclipse.desktop
+
+echo "Removendo arquivo zipado"
+rm -rf eclipse-jee-2022-03-R-linux-gtk-x86_64.tar.gz 
+
+echo "Instalando e configurando com sucesso a IDE e arquivo para usar o ECLIPSE!!!!"
